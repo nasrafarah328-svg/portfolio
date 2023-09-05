d11d21881448ddb2835b348c06eb60508b1b6341
Nasra <fara0197@algonquinlive.com>
2023-09-05 12:53:47 +0400

    update and clean up  home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,17 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./home.css";
 import { motion } from "framer-motion";
-import { FaFigma } from "react-icons/fa";
-import { FaHtml5 } from "react-icons/fa";
-import { SiAdobexd, SiAdobephotoshop } from "react-icons/si";
-import { SiCss3 } from "react-icons/si";
-import { SiJavascript } from "react-icons/si";
-import { SiReact } from "react-icons/si";
-import { SiGit } from "react-icons/si";
-import { SiSwift } from "react-icons/si";
-import { SiVisualstudio } from "react-icons/si";
-import { SiAndroid } from "react-icons/si";
-import { SlPeople } from "react-icons/sl";
 import { HiOutlineArrowLongRight } from "react-icons/hi2";
 
 function Home() {
@@ -45,11 +34,11 @@ function Home() {
             <br></br>
             <a
               className="contact-button"
-              href="https://www.linkedin.com/in/nasra-farah-7033991aa/"
+              href="https://drive.google.com/file/d/1eEjDZM7n8CBXYo0S2v_5JuLwx6UMAPC2/view?usp=sharing"
               target="_blank"
               rel="noopener noreferrer"
             >
-              Get in Touch.
+              View my CV/Resume.
             </a>
           </p>
         </div>
@@ -75,42 +64,18 @@ function Home() {
             <img src="ux.png" alt="UX/UI Design" />
             <h2 className="selection-row-sub-header">UX / UI Design</h2>
             <p className="selection-row-paragraph">
-              <FaFigma className="skills-icon" />
-              <SiAdobexd className="skills-icon" />
-              <SiAdobephotoshop className="skills-icon" />
+              
             </p>
           </div>
-          <div className="selection-row">
-            <img src="web.png" alt="Web Development" />
-            <h2 className="selection-row-sub-header">Web Development</h2>
-            <p className="selection-row-paragraph">
-              <FaHtml5 className="skills-icon" />
-              <SiCss3 className="skills-icon" />
-              <SiJavascript className="skills-icon" />
-              <SiReact className="skills-icon" />
-              <SiVisualstudio className="skills-icon" />
-              <SiGit className="skills-icon" />
-          
-            </p>
-          </div>
+    
           <div className="selection-row">
             <img src="mobile.png" alt="Mobile Development" />
-            <h2 className="selection-row-sub-header">Mobile Development</h2>
+            <h2 className="selection-row-sub-header">Mobile / Web Development</h2>
             <p className="selection-row-paragraph">
-              <SiVisualstudio className="skills-icon" />
-              <SiReact className="skills-icon" />
-              <SiSwift className="skills-icon" />
-              <SiAndroid className="skills-icon" />
+             
             </p>
           </div>
 
-          <div className="selection-row">
-            <img src="client.png" alt="Mobile Development" />
-            <h2 className="selection-row-sub-header">Client Services</h2>
-            <p className="selection-row-paragraph">
-              <SlPeople className="skills-icon" />
-            </p>
-          </div>
         </div>
       </div>
     </motion.div>
