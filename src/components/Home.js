0958444060ed5a27c94393d84b15c2ad2e435232
Nasra <fara0197@algonquinlive.com>
2023-10-24 18:17:38 -0400

    home page updates

--- src/components/Home.js
+++ src/components/Home.js
@@ -52,6 +52,11 @@ function Home() {
       <div className="selection-bottom-screen">
         <div className="selection-row-header-container">
           <h1 className="selection-row-header">My Skills</h1>
+          <p className="selection-row-text">
+            <Link to="./work">
+              view my recent work <HiOutlineArrowLongRight />{" "}
+            </Link>
+          </p>
         </div>
         <div className="selection-main-home-rows">
           <div className="selection-row">
@@ -83,14 +88,6 @@ function Home() {
             </div>
           </div>
         </div>
-        <div className="selection-row-header-container">
-          <h1 className="selection-row-header">Recent Work</h1>{" "}
-          <p className="selection-row-text">
-            <Link to="./work">
-              view my recent work <HiOutlineArrowLongRight />{" "}
-            </Link>
-          </p>
-        </div>{" "}
       </div>
     </motion.div>
   );
